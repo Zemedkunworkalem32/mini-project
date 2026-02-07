76ce17a8d087642aa891bd6d157f213fd71d179a
zemedkunworkalem32 <workalemzame@gmail.com>
2026-02-07 19:52:38 +0300

    Remove ignored files from tracking

--- middleware/errorHandler.js
+++
@@ -1,6 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-  const status = err.statusCode || 500;
-  res.status(status).json({ message: err.message || 'Server Error' });
-};
-
-module.exports = errorHandler;
